49a7be865fb9a28b1e875f96bb79781b7d72e9e0
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-04-25 19:19:35 +0000

    a

---
+++ simuladorVLP.py
@@ -0,0 +1,18 @@
+investimentoInicial = float(input("Informe o investimento inicial: \n"))
+taxaDesconto = float(input("Informe taxa de desconto: \n"))
+taxaPorcentagem = taxaDesconto / 100
+
+contador = 1
+qntFluxocaixa = int(input("Quantos anos você tem previsto de fluxo de caixa"))
+valorPresenteLiquido = 0
+
+while contador <= qntFluxocaixa:
+    faturamento = float(input(f"Digite o valor do faturamento do ano {contador}: "))
+    valorPresenteLiquido += faturamento / (1+taxaPorcentagem) ** contador
+    contador += 1
+
+
+if (valorPresenteLiquido - investimentoInicial) > 0:
+    print(f"Seuprojeto é viavel")
+else:
+    print(f"Seu projeto é inviavel")
